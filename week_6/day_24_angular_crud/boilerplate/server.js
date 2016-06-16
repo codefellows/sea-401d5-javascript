dc61b33f57abb1b0a1c3703521905140b89766f1
Rebecca Peltz <rebeccapeltz@gmail.com>
2016-06-16 13:56:27 -0700

    added boilerplate folder

---
+++ week_6/day_24_angular_crud/boilerplate/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var static  = require('express-static');
+
+var app = express();
+
+app.use(static(__dirname + '/build'));
+
+var server = app.listen(3000, function(){
+  console.log('server is running at %s', server.address().port);
+});
