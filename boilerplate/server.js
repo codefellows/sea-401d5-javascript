e3d7a84be51b1461aa80caf19daf46e71a4a03b5
Rebecca Peltz <rebeccapeltz@gmail.com>
2016-06-19 11:40:57 -0700

    update static server

--- boilerplate/server.js
+++ boilerplate/server.js
@@ -1,10 +1,7 @@
+'use strict';
 var express = require('express');
-var static  = require('express-static');
-
 var app = express();
-
-app.use(static(__dirname + '/build'));
-
-var server = app.listen(3000, function(){
+app.use(express.static(__dirname + '/build'));
+var server = app.listen(3001, function(){
   console.log('server is running at %s', server.address().port);
 });
