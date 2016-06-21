d710373107946436a6c8f0441476ac9b26ce45cf
Aaron Krause <aaronjkrause@gmail.com>
2016-06-21 16:02:46 -0700

    end inclass 6-17

--- week_7/day_27_more_directives/app/js/notes/controllers/notes_controller.js
+++ week_7/day_27_more_directives/app/js/notes/controllers/notes_controller.js
@@ -1,3 +1,4 @@
+'use strict';
 module.exports = function(app) {
   app.controller('NotesController', NotesController);
 
@@ -5,4 +6,8 @@ module.exports = function(app) {
 
 function NotesController() {
   this.notes = [{body: 'first note', _id: 2}];
-};
+  this.addNote = function(title, body) {
+    let newNote = {title, body};
+    this.notes.push(newNote);
+  };
+}
--- week_7/day_27_more_directives/app/js/notes/directives/todo.js
+++ week_7/day_27_more_directives/app/js/notes/directives/todo.js
@@ -2,8 +2,25 @@ module.exports = function(app) {
   app.directive('todoList', function() {
     return {
       templateUrl: './templates/notes/todo.html',
-      scope: {
-        notes: '='
+      scope: {notes: '='},
+      controller: function($scope) {
+        $scope.changeView = function() {
+          console.log($scope.mode);
+        };
+
+        $scope.showArticle = function(note) {
+          if (!note) {
+            $scope.mode = 'list';
+            return;
+          }
+
+          $scope.currentNote = note;
+          $scope.mode = 'single';
+        };
+
+        $scope.addNote = function(title, body) {
+          $scope.notes.push({title, body});
+        };
       }
     };
   });
