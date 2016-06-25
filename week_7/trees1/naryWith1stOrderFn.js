8652c0043ffb321283edd90c283afe399e84cecf
Rebecca Peltz <rebeccapeltz@gmail.com>
2016-06-24 18:19:06 -0700

    add in class problem solutions

--- week_7/trees1/naryWith1stOrderFn.js
+++ week_7/trees1/naryWith1stOrderFn.js
@@ -36,12 +36,32 @@ Tree.prototype.size = function() {
 Tree.prototype.exists = function(val) {
   var exists = false;
   var existsFn = function(node) { //  jshint ignore:line
-    console.log('exists',node.value);
+    console.log('exists', node.value);
     if (node.value === val) return exists = true;
 
   };
   this.traverseAndProcess(existsFn);
   return exists;
+};
+
+Tree.prototype.minMax = function() {
+  var minmax = {
+    min: Number.MAX_VALUE,
+    max: Number.MIN_VALUE
+  };
+  this.traverseAndProcess(function(node) {
+    if (node.value < minmax.min) minmax.min = node.value;
+    if (node.value > minmax.max) minmax.max = node.value;
+  });
+  return minmax;
+};
+
+Tree.prototype.toArray = function() {
+  var arr = [];
+  this.traverseAndProcess(function(node) {
+    arr.push(node.value);
+  });
+  return arr;
 };
 
 //Test my tree functions
@@ -65,3 +85,20 @@ console.log('size of tree:', a.size());
 console.log('exists a', a.exists('a'));
 console.log('exists b', a.exists('b'));
 console.log('exists z', a.exists('z'));
+
+console.log('toArray', a.toArray());
+
+var a1 = new Tree(-1);
+var a2 = new Tree(2);
+var a3 = new Tree(3);
+var a4 = new Tree(4);
+var a5 = new Tree(12);
+var a6 = new Tree(100);
+a1.children.push(a2);
+a1.children.push(a3);
+a1.children.push(a4);
+a2.children.push(a5);
+a5.children.push(a6);
+
+var minmax = a1.minMax();
+console.log('min', minmax.min, 'max', minmax.max);
